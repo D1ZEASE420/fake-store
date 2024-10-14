23eca7aa672d997b16a6baf21f0db9412e6b5102
Gen Vikat <gen.vikat@ametikool.ee>
2024-10-14 11:40:17 +0300

    Add data to single product page, configure history state of router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory , createRouter } from 'vue-router'
 
 import ProductList from './views/ProductList.vue'
 import SingleProduct from './views/SingleProduct.vue'
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(), 
+  history: createWebHistory(), 
   routes,
 })
 
